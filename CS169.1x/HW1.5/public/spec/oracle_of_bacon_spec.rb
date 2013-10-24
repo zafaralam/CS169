76b6d01e294821e6d12a0783f142c57408e50074
zafaralam <fastalam420@gmail.com>
2013-10-24 22:12:43 +1100

    working on uri part

--- CS169.1x/HW1.5/public/spec/oracle_of_bacon_spec.rb
+++ CS169.1x/HW1.5/public/spec/oracle_of_bacon_spec.rb
@@ -36,7 +36,7 @@ describe OracleOfBacon do
       end
     end
   end
-  describe 'parsing XML response' do
+  describe 'parsing XML response', :pending => true do
     describe 'for unauthorized access/invalid API key' do
       subject { OracleOfBacon::Response.new(File.read 'spec/unauthorized_access.xml') }
       its(:type) { should == :error }
@@ -67,7 +67,7 @@ describe OracleOfBacon do
       its(:data) { should match /unknown/i }
     end
   end
-  describe 'constructing URI', :pending => true do
+  describe 'constructing URI' do
     subject do
       oob = OracleOfBacon.new('fake_key')
       oob.from = '3%2 "a' ; oob.to = 'George Clooney'
